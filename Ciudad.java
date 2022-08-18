40c92bd6261a057978ed09d5dda0e884691d0cce
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2022-08-17 19:54:33 -0500

    First commit

---
+++ Ciudad.java
@@ -0,0 +1,14 @@
+public class Ciudad {
+    public String id;
+    public String nombreCiudad;
+    public String estado;
+    public String numeroHabitantes;
+
+    public Ciudad(String id, String nombreCiudad, String estado, String numeroHabitantes) {
+        this.id = id;
+        this.nombreCiudad = nombreCiudad;
+        this.estado = estado;
+        this.numeroHabitantes = numeroHabitantes;
+    }
+
+}
