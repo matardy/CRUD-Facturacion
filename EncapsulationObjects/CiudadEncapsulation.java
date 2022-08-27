51fc4d7d9cc6626ea1ab08238ba24e2718ced891
matardy <gutemberg.mendoza@epn.edu.ec>
2022-08-27 14:12:22 -0500

    Documentacion de la clase CiudadEncapsulation

--- EncapsulationObjects/CiudadEncapsulation.java
+++ EncapsulationObjects/CiudadEncapsulation.java
@@ -1,11 +1,24 @@
 package EncapsulationObjects;
 
+
+/**
+ * Clase creada para encapsular los dados de la tabla cidad
+ */
 public class CiudadEncapsulation {
+
     public String id;
     public String nombreCiudad;
     public String estado;
     public String numeroHabitantes;
 
+
+    /**
+     * Constructor de clase
+     * @param id
+     * @param nombreCiudad
+     * @param estado
+     * @param numeroHabitantes
+     */
     public CiudadEncapsulation(String id, String nombreCiudad, String estado, String numeroHabitantes) {
         this.id = id;
         this.nombreCiudad = nombreCiudad;
